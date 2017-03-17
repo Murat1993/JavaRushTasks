f4f7a233f46af14b6fe620dd414243542e27b35d
Murat1993 <muratbek199325@gmail.com>
2017-03-17 10:55:07 +0600

    solved Refactoring(5)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
@@ -13,6 +13,11 @@ public class Student extends UniversityPerson {
         this.averageGrade = averageGrade;
     }
 
+    @Override
+    public String getPosition() {
+        return "Студент";
+    }
+
     public void live() {
         learn();
     }
@@ -24,10 +29,6 @@ public class Student extends UniversityPerson {
         return course;
     }
 
-
-    public void printData() {
-        System.out.println("Студент: " + name);
-    }
 
     public void incAverageGradeBy01() {
         averageGrade += 0.1;
