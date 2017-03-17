7716a4723e34b785a1f6b3636d74eafe66bdd9be
Murat1993 <muratbek199325@gmail.com>
2017-03-17 12:47:55 +0600

    solved Refactoring(7)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
@@ -3,21 +3,18 @@ package com.javarush.task.task29.task2909.human;
 import java.util.Date;
 
 /*
-6.1. Замена параметра набором специализированных методов. Замени метод setValue() класса
-Student специализированными методами setCourse и setAverageGrade.
+7.1. Параметризация метода.
+Замени методы incAverageGradeBy01() и incAverageGradeBy02() одним методом incAverageGrade(double delta).
 
-6.2. Добавление параметра.
-Добавить параметр с типом double в метод getStudentWithAverageGrade(),
- чтобы было понятно с каким средним балом нужен студент.
-Реализуй метод getStudentWithAverageGrade().
+7.2. Передача всего объекта. Перепиши метод addInfoAboutStudent(),
+чтобы он в качестве параметра принимал объект типа Student.
 
-6.3. Удаление параметра. Убери параметр из метода getStudentWithMaxAverageGrade().
-Реализуй этот метод, он должен возвращать студента с максимальным средним балом.
+7.3. Замена параметра вызовом метода. Перепиши метод printInfoAboutStudent(),
+чтобы он не требовал в качестве параметра имя студента, а получал его,
+вызвав соответствующий метод у переданного объекта.
 
-6.4. Разделение запроса и модификатора.
-Раздели метод getStudentWithMinAverageGradeAndExpel на Student getStudentWithMinAverageGrade()
-и void expel(Student student). Первый метод должен возвратить студента с минимальным
- средним балом, а второй — отчислить переданного студента (удалять из списка students).
+7.4. Замена параметров объектом. Перепиши методы setBeginningOfSession и setEndOfSession,
+чтобы они вместо набора параметров принимали по одному объекту даты.
 * */
 
 public class Student extends UniversityPerson {
@@ -50,20 +47,18 @@ public class Student extends UniversityPerson {
         this.course = course;
     }
 
-    public void incAverageGradeBy01() {
-        averageGrade += 0.1;
+    public void incAverageGrade(double delta) {
+        averageGrade += delta;
     }
 
-    public void incAverageGradeBy02() {
-        averageGrade += 0.2;
+
+
+    public void setBeginningOfSession(Date date) {
+        beginningOfSession = date;
     }
 
-    public void setBeginningOfSession(int day, int month, int year) {
-        beginningOfSession = new Date(year, month, day);
-    }
-
-    public void setEndOfSession(int day, int month, int year) {
-        endOfSession = new Date(year, month, day);
+    public void setEndOfSession(Date date) {
+        endOfSession = date;
     }
 
     public double getAverageGrade() {
--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
@@ -1,22 +1,6 @@
 package com.javarush.task.task29.task2909.human;
 
 
-/*
-4.1. Замена наследования делегированием.
-4.1.1. Класс University не должен наследоваться от Student.
-4.1.2. Класс University должен содержать список students. Не забудь его инициализировать.
-4.1.3. Добавь сеттер и геттер для students.
-4.1.4. Университет имеет название (name) и возраст (age). Добавь необходимые поля,
-сеттеры и геттеры для них.
-
-4.2. Извлечение суперкласса.
-4.2.1. Создай класс UniversityPerson в пакете human.
-4.2.2. Перенеси в него поле university.
-4.2.3. Перенеси сеттер и геттер для поля university.
-4.2.4. Унаследуй необходимые классы от UniversityPerson.
-4.3. Замена простого поля объектом. Измени тип поля university на University.
-* */
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -54,12 +38,6 @@ public class University {
         this.students = students;
     }
 
-    /*
-    6.2. Добавление параметра.
-    Добавить параметр с типом double в метод getStudentWithAverageGrade(),
-    чтобы было понятно с каким средним балом нужен студент.
-    Реализуй метод getStudentWithAverageGrade().*/
-
     public Student getStudentWithAverageGrade(double averageGrade) {
         for (Student student : students) {
             if (student.getAverageGrade() == averageGrade) {
@@ -68,9 +46,6 @@ public class University {
         }
         return null;
     }
-/*
-    6.3. Удаление параметра. Убери параметр из метода getStudentWithMaxAverageGrade().
-    Реализуй этот метод, он должен возвращать студента с максимальным средним балом.*/
 
     public Student getStudentWithMaxAverageGrade() {
         double maxAverage = students.get(0).getAverageGrade();
@@ -83,12 +58,6 @@ public class University {
         }
         return studentWithMaxAverage;
     }
-/*
-    6.4. Разделение запроса и модификатора.
-    Раздели метод getStudentWithMinAverageGradeAndExpel
-    на Student getStudentWithMinAverageGrade()
-    и void expel(Student student). Первый метод должен возвратить студента с минимальным
-    средним балом, а второй — отчислить переданного студента (удалять из списка students).*/
 
     public Student getStudentWithMinAverageGrade() {
         double minAverage = students.get(0).getAverageGrade();
