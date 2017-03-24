d1bb7415bd66b927b647b27f8261b9d20b841262
Murat1993 <muratbek199325@gmail.com>
2017-03-24 09:49:44 +0600

    solved HTML Editor (2)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,26 +1,25 @@
 package com.javarush.task.task32.task3209;
 
 /*
-* 1.1. Объяви класс Controller и класс View.
-* Класс View должен быть унаследован от JFrame и реализовывать интерфейс ActionListener.
+* 2.1. Добавь в контроллер и представление по методу init(), пока без реализаций.
+* Они будут отвечать за инициализацию контроллера и представления.
 
-1.2. Добавь в класс Controller поля,
-отвечающие за представление View view и модель HTMLDocument document.
-Здесь и далее, классы, которые мы не объявляли,
-но используем в коде скорее всего все находятся в библиотеке swing.
-Например, класс HTMLDocument реализован в пакете javax.swing.text.html.
+2.2. Теперь напишем в классе Controller метод main(). Он должен:
 
-1.3. Добавь в класс Controller поле, которое будет отвечать за файл,
-который сейчас открыт в нашем редакторе (текущий файл) File currentFile.
+2.2.1. Создавать объект представления.
 
-1.4. Добавь конструктор класса Controller.
-Он должен принимать в качестве параметра представление и инициализировать
- соответствующее поле класса.
+2.2.2. Создавать контроллер, используя представление.
 
-1.5. Добавь в Controller пустой метод main.
-1.6. Добавь в класс View поле Controller controller.
-1.7. Добавь в класс View сеттер и геттер для поля controller.
-1.8. Добавь пустую реализацию метода, который объявлен в интерфейсе ActionListener.*/
+2.2.3. Устанавливать у представления контроллер.
+
+2.2.4. Инициализировать представление.
+
+2.2.5. Инициализировать контроллер. Контроллер должен инициализироваться после представления.
+
+2.3. Добавь в контроллер метод exit(), он должен вызывать статический метод exit у класса
+System.
+
+2.4. Добавь в представление метод exit(), он должен вызывать exit() у контроллера.*/
 
 import javax.swing.text.html.HTMLDocument;
 import java.io.File;
@@ -30,11 +29,23 @@ public class Controller {
     private HTMLDocument document;
     private File currentFile;
 
+    public static void main(String[] args) {
+        View view = new View();
+        Controller controller = new Controller(view);
+        view.setController(controller);
+        view.init();
+        controller.init();
+    }
+
     public Controller(View view) {
         this.view = view;
     }
 
-    public static void main(String[] args) {
+    public void init() {
 
+    }
+
+    public void exit() {
+        System.exit(0);
     }
 }
