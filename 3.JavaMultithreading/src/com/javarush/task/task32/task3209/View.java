14735172f439030647c5d1c96e95109bf6d0491e
Murat1993 <muratbek199325@gmail.com>
2017-03-24 16:34:07 +0600

    solved HTML Editor (6)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,27 +1,13 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/*
-4.3. Реализуй метод init() представления. Он должен:
-
-4.3.1. Вызывать инициализацию графического интерфейса initGui().
-
-4.3.2. Добавлять слушателя событий нашего окна.
-В качестве подписчика создай и используй объект класса FrameListener.
- В качестве метода для добавления подписчика используй подходящий метод из
- класса Window от которого наследуется и наш класс через классы JFrame и Frame.
-
-4.3.3. Показывать наше окно. Используй метод setVisible с правильным параметром.
-На этом этапе приложение при запуске должно показывать окно,
-которое можно растягивать, разворачивать, закрыть и т.д.
-
-
-* */
 
 public class View extends JFrame implements ActionListener {
     private Controller controller;
@@ -45,12 +31,44 @@ public class View extends JFrame implements ActionListener {
     public void initMenuBar() {
 
     }
+    /*Реализуй метод инициализации панелей редактора initEditor(). Он должен:
 
-    public void selectedTabChanged() {
+6.1. Устанавливать значение «text/html» в качестве типа контента для компонента htmlTextPane.
+Найди и используй подходящий метод. +
 
-    }
+6.2. Создавать новый локальный компонент JScrollPane на базе htmlTextPane. +
+
+6.3. Добавлять вкладку в панель tabbedPane с именем «HTML» и компонентом из предыдущего
+пункта. +
+
+6.4. Создавать новый локальный компонент JScrollPane на базе plainTextPane.
+
+6.5. Добавлять еще одну вкладку в tabbedPane с именем «Текст» и компонентом из
+предыдущего пункта. +
+
+6.6. Устанавливать предпочтительный размер панели tabbedPane.
+
+6.7. Создавать объект класса TabbedPaneChangeListener и
+устанавливать его в качестве слушателя изменений в tabbedPane.
+
+6.8. Добавлять по центру панели контента текущего фрейма нашу панель с вкладками.
+Получить панель контента текущего фрейма можно с помощью метода getContentPane(),
+его реализация унаследовалась от JFrame.
+После запуска приложения можно будет увидеть текущие результаты:
+две независимые закладки (HTML и Текст), в каждой из которых можно набирать свой текст.
+    * */
 
     public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+        tabbedPane.addTab("HTML", new JScrollPane(htmlTextPane));
+        tabbedPane.addTab("Текст", new JScrollPane(plainTextPane));
+        tabbedPane.setPreferredSize(new Dimension(300, 300));
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
+
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
+    }
+
+    public void selectedTabChanged() {
 
     }
 
