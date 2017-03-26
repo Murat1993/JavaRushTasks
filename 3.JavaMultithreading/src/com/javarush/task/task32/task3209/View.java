9aa82a1b1af0e20a1d6c0e46e32a0aadd926df9c
Murat1993 <muratbek199325@gmail.com>
2017-03-26 10:37:31 +0600

    solved HTML Editor (9)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -8,12 +8,22 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static com.javarush.task.task32.task3209.MenuHelper.*;
+
 
 public class View extends JFrame implements ActionListener {
     private Controller controller;
     private JTabbedPane tabbedPane = new JTabbedPane();
     private JTextPane htmlTextPane = new JTextPane();
     private JEditorPane plainTextPane = new JEditorPane();
+
+    public View() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
 
     public void init() {
         initGui();
@@ -28,34 +38,56 @@ public class View extends JFrame implements ActionListener {
         pack();
     }
 
-    public void initMenuBar() {
+    /*9.1. Реализуй метод initMenuBar(). Он должен:
 
-    }
-    /*Реализуй метод инициализации панелей редактора initEditor(). Он должен:
+9.1.1. Создавать новый объект типа JMenuBar. Это и будет наша панель меню. +
 
-6.1. Устанавливать значение «text/html» в качестве типа контента для компонента htmlTextPane.
-Найди и используй подходящий метод. +
+9.1.2. С помощью MenuHelper инициализировать меню в следующем порядке: Файл,
+Редактировать, Стиль, Выравнивание, Цвет, Шрифт и Помощь.
 
-6.2. Создавать новый локальный компонент JScrollPane на базе htmlTextPane. +
-
-6.3. Добавлять вкладку в панель tabbedPane с именем «HTML» и компонентом из предыдущего
-пункта. +
-
-6.4. Создавать новый локальный компонент JScrollPane на базе plainTextPane.
-
-6.5. Добавлять еще одну вкладку в tabbedPane с именем «Текст» и компонентом из
-предыдущего пункта. +
-
-6.6. Устанавливать предпочтительный размер панели tabbedPane.
-
-6.7. Создавать объект класса TabbedPaneChangeListener и
-устанавливать его в качестве слушателя изменений в tabbedPane.
+9.1.3. Добавлять в верхнюю часть панели контента текущего фрейма нашу панель меню,
+аналогично тому, как это мы делали с панелью вкладок.
 
 6.8. Добавлять по центру панели контента текущего фрейма нашу панель с вкладками.
 Получить панель контента текущего фрейма можно с помощью метода getContentPane(),
 его реализация унаследовалась от JFrame.
 После запуска приложения можно будет увидеть текущие результаты:
-две независимые закладки (HTML и Текст), в каждой из которых можно набирать свой текст.
+две независимые закладки (HTML и Текст),
+в каждой из которых можно набирать свой текст.
+
+
+9.2. Добавь конструктор класса View.
+Он должен устанавливать внешний вид и поведение (look and feel) нашего приложения такими же,
+как это определено в системе.
+Конструктор не должен кидать исключений, только логировать их с помощью ExceptionHandler.
+
+Подсказа: для реализации задания используй класс UIManager.
+
+Запусти приложение, теперь ты должен видеть панель с меню вверху окна.
+Некоторые из пунктов меню (например: Вырезать, Копировать, Вставить, Стиль (частично),
+Выравнивание, Цвет, Шрифт) должны уже работать.
+Убедись, что все работает и только затем продолжи разработку.
+
+    * */
+
+    public void initMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        initFileMenu(this, menuBar);
+        initEditMenu(this, menuBar);
+        initStyleMenu(this, menuBar);
+        initAlignMenu(this, menuBar);
+        initColorMenu(this, menuBar);
+        initFontMenu(this, menuBar);
+        initHelpMenu(this, menuBar);
+        getContentPane().add(menuBar, BorderLayout.NORTH);
+    }
+
+    /*6.8. Добавлять по центру панели контента текущего фрейма нашу панель с вкладками.
+Получить панель контента текущего фрейма можно с помощью метода getContentPane(),
+ его реализация унаследовалась от JFrame.
+После запуска приложения можно будет увидеть текущие результаты:
+две независимые закладки (HTML и Текст),
+ в каждой из которых можно набирать свой текст.
     * */
 
     public void initEditor() {
