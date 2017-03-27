a062907b9947c53b9e909e72b342d30a849df733
Murat1993 <muratbek199325@gmail.com>
2017-03-27 12:27:33 +0600

    solved HTML editor(17)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,13 +1,14 @@
 package com.javarush.task.task32.task3209;
 
 /*
-*Добавь метод setPlainText(String text) в контроллер. Он будет записывать переданный
-* текст с html тегами в документ document. При его реализации:
-16.1. Сбрось документ.
-16.2. Создай новый реадер StringReader на базе переданного текста. +
-16.3. Вызови метод read() из класса HTMLEditorKit, который вычитает данные из реадера в
-документ document.
-16.4. Проследи, чтобы метод не кидал исключения. Их необходимо просто логировать.
+*Добавь метод String getPlainText() в контроллер.
+* Он должен получать текст из документа со всеми html тегами.
+17.1. Создай объект StringWriter.
+17.2. Перепиши все содержимое из документа document в созданный объект с помощью
+метода write класса HTMLEditorKit.
+17.3. Как обычно, метод не должен кидать исключений.
+
+
 */
 
 import com.javarush.task.task32.task3209.listeners.UndoListener;
@@ -18,6 +19,7 @@ import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.io.StringWriter;
 
 public class Controller {
     private View view;
@@ -34,6 +36,18 @@ public class Controller {
         view.setController(controller);
         view.init();
         controller.init();
+    }
+
+    public String getPlainText() {
+        StringWriter writer = new StringWriter();
+        try {
+            new HTMLEditorKit().write(writer, document, 0, document.getLength());
+        } catch (IOException e) {
+            ExceptionHandler.log(e);
+        } catch (BadLocationException e) {
+            ExceptionHandler.log(e);
+        }
+        return writer.toString();
     }
 
     public void setPlainText(String text) {
