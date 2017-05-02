1a43e31a60e2601142c0a0cadfc19d084c8b6109
Murat1993 <muratbek199325@gmail.com>
2017-05-02 19:39:33 +0600

    solved task32.task3203

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -3,40 +3,63 @@ package com.javarush.task.task20.task2028;
 import java.io.Serializable;
 import java.util.AbstractList;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
 /* 
-Класс описывающий дерево мы создали, теперь нужен класс описывающий тип элементов дерева:
-1.  В классе CustomTree создай вложенный статический параметризированный
-класс Entry<T> с модификатором доступа по умолчанию. +
+Итак, основа дерева создана, пора тебе поработать немного самому.
+Вспомним как должно выглядеть наше дерево.
 
-2. Обеспечь поддержку этим классом интерфейса Serializable. +
+Элементы дерева должны следовать так как показано на картинке:
+http://info.javarush.ru/uploads/images/00/04/89/2014/03/21/ee9a9b.jpg
 
-3. Создай такие поля (модификатор доступа по умолчанию):
-— String elementName;
-— int lineNumber;
-— boolean availableToAddLeftChildren, availableToAddRightChildren;
-— Entry<T> parent, leftChild, rightChild; +
+Необходимо написать методы,
+которые бы позволили создать такую структуру дерева и проводить операции над ней.
 
-4. Реализуй публичный конструктор с одним параметром типа String:
-— установи поле elementName равным полученному параметру;
-— установи поле availableToAddLeftChildren равным true;
-— установи поле availableToAddRightChildren равным true; +
 
-5. Реализуй метод void checkChildren, устанавливающий поле availableToAddLeftChildren в false,
- если leftChild не равен null и availableToAddRightChildren в false,
-  если rightChild не равен null. +
+Требования:
+1. После добавления N элементов в дерево с помощью метода add, метод size должен возвращать N.
+2. После удаления последнего добавленного элемента из дерева с помощью метода remove,
+метод size должен возвращать N-1.
+3. После удаления второго элемента добавленного в дерево,
+метод size должен возвращать N/2 + 1 (для случаев где N > 2 и является степенью двойки),
+N - размер дерева до удаления.
 
-6. Реализуй метод boolean isAvailableToAddChildren,
-возвращающий дизъюнкцию полей availableToAddLeftChildren и availableToAddRightChildren.
+4. Метод getParent должен возвращать имя родителя для любого элемента дерева.
 
-Любое дерево начинается с корня,
- поэтому не забудь в класс CustomTree добавить поле root типа Entry<String> c модификатором доступа по умолчанию.
 
+Тебе необходимо реализовать:
+1. метод add(String s) — добавляет элементы дерева,
+ в качестве параметра принимает имя элемента (elementName),
+ искать место для вставки начинаем слева направо.
+
+2. метод remove(String s) — удаляет элемент дерева имя которого было полученного
+в качестве параметра.
+
+3. метод size() — возвращает текущее количество элементов в дереве.
+
+4. метод getParent(String s) — возвращает имя родителя элемента дерева,
+имя которого было полученного в качестве параметра.
 
 */
-public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
+public class CustomTree extends AbstractList<String>
+        implements Cloneable, Serializable {
     Entry<String> root;
+
+    public boolean add(String s) {
+        Entry<String> newEntry = new Entry<>(s);
+        if (root == null) {
+            root = newEntry;
+        }
+
+        while (true) {
+            if (root.isAvailableToAddChildren()) {
+
+            }
+        }
+    }
+
+
 
     static class Entry<T> implements Serializable {
         String elementName;
@@ -117,6 +140,10 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
 
     @Override
     public int size() {
-        return 0;
+        int size = 0;
+        if (root == null) {
+            size = 0;
+        }
+        return size;
     }
 }
