c4cf499b90c01e0541321c8e78a8d66865a8a088
Murat1993 <muratbek199325@gmail.com>
2017-06-08 11:08:09 +0600

    solved space(11)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
@@ -1,7 +1,33 @@
 package com.javarush.task.task25.task2515;
 
+/*
+Теперь напишем класс Rocket.
+Практически совпадает с классом Bomb.
+Только:
+а) Ракета летит вверх (т.е. y уменьшается на 1);
+б) Рисуем не букву «B«, а букву «R«.
+
+
+* */
+
 public class Rocket extends BaseObject {
-    public Rocket(double x, double y, double radius) {
-        super(x, y, radius);
+    public Rocket(double x, double y) {
+        super(x, y, 1);
+    }
+
+    /**
+     * Отрисовываем себя на холсте.
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.setPoint(x, y, 'R');
+    }
+
+    /**
+     * Двигаем себя вниз на один ход.
+     */
+    @Override
+    public void move() {
+        y--;
     }
 }
